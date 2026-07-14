1d6a08317eb9df97497aa9c0953286703d9a5c41
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-07-14 22:06:35 +0900

    TransformをCoreから、balloonEngineLowへ移動

---
+++ Game/balloonEngineLow/Transform.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file Transform.cpp
+ * @brief 変換情報を保持するクラスの実装
+ */
+#include "stdafx.h"
+#include "Transform.h"
+
+
+namespace BalloonEngineLow
+{
+	Transform::Transform(Vector3 position, Quaternion rotation, Vector3 scale)
+		: m_position(position)
+		, m_rotation(rotation)
+		, m_scale(scale)
+	{}
+
+
+	Transform::~Transform()
+	{}
+}
