8a63e3b7de18bbb7c49253c7ece1dc5c6c9151fb
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-07-15 01:19:05 +0900

    Step1_6 (#7)

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -8,7 +8,6 @@ bool Game::Start()
 {
 	// Load resources and set up your objects here (called once).
 
-	auto& light = balloonEngineLow::LightManager::Get();
 
 	m_modelRender.Init("Assets/modelData/unityChan.tkm", enModelUpAxisZ);
 	m_modelRender.SetPosition(Vector3(-100.0f, 0.0f, 0.0f));
@@ -17,6 +16,15 @@ bool Game::Start()
 	m_modelRender.SetRotation(rot);
 	m_modelRender.SetScale(Vector3(1.5f, 1.5f, 1.5f));
 	m_modelRender.Update();
+
+	const Vector3 modelPos = m_modelRender.GetTransform().m_position;
+	const Vector3 lightPos = modelPos + Vector3(100.0f, 0.0f, 0.0f);
+
+	auto& light = balloonEngineLow::LightManager::Get();
+	light.SetLightColor(g_vec4Yellow);
+	light.SetLightDir(modelPos - lightPos);
+
+
 	return true;
 }
 
