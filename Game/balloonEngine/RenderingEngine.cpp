f58cd1b798a7009897aa4ffe6a287ec09a7002dd
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2026-07-16 12:26:13 +0900

    レンダリングエンジンクラス追加

---
+++ Game/balloonEngine/RenderingEngine.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file RenderingEngine.cpp
+ * @brief 描画エンジンクラスの実装
+ */
+#include "stdafx.h"
+
+#include "RenderingEngine.h"
+
+
+namespace balloonEngine
+{
+
+}
---
+++ Game/balloonEngine/RenderingEngine.h
@@ -0,0 +1,19 @@
+/**
+ * @file RenderingEngine.h
+ * @brief 描画エンジンクラスの宣言
+ */
+#pragma once
+
+namespace balloonEngine
+{
+    /**
+     * @brief 描画エンジンクラス
+     */
+    class RenderingEngine
+    {
+
+      private:
+        RenderingEngine();
+        ~RenderingEngine() = default;
+    };
+} // namespace balloonEngine
